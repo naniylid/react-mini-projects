81ec779b924efb8465826814a8d1bfd5497eb518
Nadezhda Ilina <nadiailina9@gmail.com>
2023-12-18 15:18:02 +0300

    React Router и скелетоны

--- react/pizza/src/App.js
+++ react/pizza/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './scss/app.scss';
 import Header from './components/Header';
-import Categories from './components/Categoties';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
-
-import pizzas from './assets/pizza.json/pizzas-list.json';
+import { Home } from './pages/Home';
+import Cart from './pages/Cart';
+import { NotFound } from './pages/NotFound';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -13,16 +12,11 @@ function App() {
       <Header />
       <div className='content'>
         <div className='container'>
-          <div className='content__top'>
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className='content__title'>Все пиццы</h2>
-          <div className='content__items'>
-            {pizzas.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
-          </div>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ react/pizza/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Categories from '../components/Categoties';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+export const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://65746aecf941bda3f2afba20.mockapi.io/pizza')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      <div className='content__top'>
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className='content__title'>Все пиццы</h2>
+      <div className='content__items'>
+        {isLoading
+          ? [...new Array(12)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>{' '}
+    </>
+  );
+};
